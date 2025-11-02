86d9ab35009907d8d60fe32c35c52909f8208f7e
Sujal <sujalagarwal0910@gmail.com>
2025-11-02 06:52:10 +0530

    Fix: Remove gevent and change worker class to sync

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,6 @@ import multiprocessing
 # Server settings
 bind = "0.0.0.0:10000"  # Render will connect to this port
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+worker_class = "sync"  # <-- THIS IS THE FIX
 preload_app = True
+
